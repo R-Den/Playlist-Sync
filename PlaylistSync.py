2d8810484d84065710841da6b30c1cedad243d4a
Arden <ace001.as@gmail.com>
2020-07-17 22:54:11 +1000

    what i have done for today

--- PlaylistSync.py
+++ PlaylistSync.py
@@ -1,6 +1,11 @@
+from secrets import spotify_user_id, spotify_token
+import json
+import
+
+
 class CreatePlaylist:
     def __init__(self):
-        pass
+        self.user_id = spotify_user_id
 
     def get_youtube_Client(self):
         pass
@@ -9,9 +14,22 @@ class CreatePlaylist:
         pass
 
     def create_playlist(self):
-        pass
+        request_body = json.dumps({
+            "name": "Youtube Liked Vids",
+            "description": "All Liked Youtube Videos",
+            "public": false
+        })
 
-    def get_spotify_uri(pass):
+        query = "https://api.spotify.com/v1/users/{}/playlists".format(
+            self.user_id)
+        response = requests.post(
+            query,
+            data=request_body,
+            headers={"Content-Type": "application/json",
+                     "Authorization": "Bearer {}".format(spotify_token)}
+        )
+
+    def get_spotify_uri(self):
         pass
 
     def add_song_to_playlist(self):
