7299a065545a203821ca6326560596e7e1822818
Arden <ace001.as@gmail.com>
2020-07-17 22:13:34 +1000

    added steps

---
+++ PlaylistSync.py
@@ -0,0 +1,18 @@
+class CreatePlaylist:
+    def __init__(self):
+        pass
+
+    def get_youtube_Client(self):
+        pass
+
+    def get_liked_video(self):
+        pass
+
+    def create_playlist(self):
+        pass
+
+    def get_spotify_uri(pass):
+        pass
+
+    def add_song_to_playlist(self):
+        pass
